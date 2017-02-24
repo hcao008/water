216021621b2e80e76a3734d2502867c432df7bf8
Hilary Cao <hcao@certussolutions.com>
2017-02-25 10:59:53 +1100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  #Add About page routes
+get 'about', to: 'home#about'
+
+  root 'home#index'
+get 'home/index'
+
+  resources :charges
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
